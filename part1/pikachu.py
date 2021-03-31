1de45098782f4ebaa0e42494cde7f464f2295e5f
Vijay Iyer <vsiyer@iu.edu>
2021-03-30 23:37:51 -0400

    Evaluation function  - pikachu game

--- part1/pikachu.py
+++ part1/pikachu.py
@@ -24,8 +24,8 @@ import copy
 
 def EvaluateState(State, player):
     if player == 'w':
-        return len(State.b_pieces) + sum([i.position[1] for i in State.w_pieces]) - sum([i.position[1] for i in State.b_pieces])
-    return len(State.w_pieces) + sum([i.position[1] for i in State.b_pieces]) - sum([i.position[1] for i in State.w_pieces])
+        return len(State.b_pieces) + sum([len(State.board)*i if type(i) is Pikachu else i for i in State.b_pieces])
+    return len(State.w_pieces) + sum([len(State.board)*i if type(i) is Pikachu else i for i in State.w_pieces])
 
 class Move:
     def __init__(self, previous, current, captures):
